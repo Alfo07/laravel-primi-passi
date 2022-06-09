4d3b99bd06d4a05b491b32dc00984b80a3ac7f68
Alfo07 <ammiratialfonso07@gmail.com>
2022-06-09 18:38:09 +0200

    Esercizio completo

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/user', function () {
+    return view('user');
+})->name('user');
+
+Route::get('/platform', function () {
+    return view('platform');
+})->name('platform');
